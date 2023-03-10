060a0320c2111674f756592dde297fe531acf952
German Garcia <gmgarcia@ucundinamarca.edu.co>
2023-03-10 17:29:12 -0500

    Names

--- capitulo1.2-valores-salida/main.tf
+++ capitulo1.2-valores-salida/main.tf
@@ -1,11 +1,11 @@
 resource "random_string" "rg-name" {
   length  = 12
   upper   = true
-  number  = false
+  numeric = false
   lower   = true
   special = false
 }
 
 output "groupName" {
-    value = random_string.rg-name.result
+  value = random_string.rg-name.result
 }
--- capitulo1.3/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-#información común
-group_name           = "terraformNewGroup"
-storage_account_name = "storagelibroamin"
-group_location       = "westus"
-network_name         = "libronetwork"
-subnet_name          = "subnet1"
