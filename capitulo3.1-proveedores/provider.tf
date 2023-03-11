d4783f93bdefcf0aeb744c833908663153f339f6
German Garcia <gmgarcia@ucundinamarca.edu.co>
2023-03-10 21:51:13 -0500

    capitulo3.1-proveedores

---
+++ capitulo3.1-proveedores/provider.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  subscription_id = "subscription ID"
+  client_id       = "client ID"
+  client_secret   = "client secret"
+  tenant_id       = "tenant ID"
+  features {}
+}
+
+provider "random" {
+
+}
