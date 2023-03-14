34c46bf2bd8cbb359d449835734c645a085ae3cc
German Garcia <gmgarcia@ucundinamarca.edu.co>
2023-03-13 21:43:49 -0500

    capitulo5.1-tags

---
+++ capitulo5.1-tags/main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {
+
+  }
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "devResourceGroup"
+  location = "eastus"
+  tags = {
+    "Environment" = "devResourceGroup"
+  }
+}
