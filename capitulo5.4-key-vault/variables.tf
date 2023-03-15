d3f1643c82db6dd87b345148c3aff14bb3c40ceb
German Garcia <gmgarcia@ucundinamarca.edu.co>
2023-03-14 20:08:41 -0500

    capitulo5.4-key-vault

---
+++ capitulo5.4-key-vault/variables.tf
@@ -0,0 +1,5 @@
+variable "keyVaultSecretValue" {
+  type        = string
+  description = "Where to put the value from KeyVault"
+  default     = "/subscriptions/19a46161-9546-4b65-8c7c-01ab4562c77b/resourceGroups/stateResourceGroup/providers/Microsoft.KeyVault/vaults/terraformvaultlibro"
+}
